26d3a1c64cc02c374d8c7bbc641f3ce4a747a142
emre.mutlu <emre.mutlu@bilyoner.com>
2024-02-24 00:41:03 +0300

    htmlFor edit

--- src/app/characters/page.js
+++ src/app/characters/page.js
@@ -64,7 +64,7 @@ const Characters = () => {
           <div>
             <form className="max-w-sm mx-auto" onSubmit={onSubmit}>
               <label
-                for="website-admin"
+                htmlFor="website-admin"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Name
@@ -90,7 +90,7 @@ const Characters = () => {
                   onChange={(e) => setFirstName(e.target.value)}
                 />
               </div>
-              <button type="submit" class="text-white bg-slate-400  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Search</button>
+              <button type="submit" className="text-white bg-slate-400  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Search</button>
 
             </form>
           </div>
